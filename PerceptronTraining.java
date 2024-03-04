4db7c4fe6ebea1d000bf8565b17702519bb5088d
Kieran Kennedy <kierankennedy@sandiego.edu>
2024-03-03 21:39:19 -0800

    Implemented reading in data from training file

--- PerceptronTraining.java
+++ PerceptronTraining.java
@@ -1,7 +1,9 @@
-
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class PerceptronTraining {
-	int weight, epoch;
+	int weight, epoch, inputDimension, outputDimension, numPairs;
+	BufferedReader reader;
 	double learningRate, theta, threshold;
 	String readFile, writeFile;
 		
@@ -14,14 +16,62 @@ public class PerceptronTraining {
 		this.theta = theta;
 		this.threshold = threshold;
 
-		BufferedReader reader = new BufferedReader(new FileReader(readFile));
-                int inputDimension = reader.readLine();
-                int outputDimension = reader.readLine();
-                int numPairs = reader.readLine()
+		try{
+			reader = new BufferedReader(new FileReader(readFile)); //CLOSE READER AT SOME POINT
+			inputDimension = Integer.parseInt(reader.readLine());
+			outputDimension = Integer.parseInt(reader.readLine());
+			numPairs = Integer.parseInt(reader.readLine());
+		}catch(Exception e){
+			System.out.println("ERROR: " + e);
+		}
+		
+		System.out.println("Input Dimension: " + inputDimension + "\nOutput Dimension: " + outputDimension + "\nNumber of Pairs: " + numPairs); //REMOVE LATER
 	}
 
 	// implement main training algorithm and call any necessary helper methods (do 1 thing per method)
 	public void Train() {
+		int[] inputArr = getInputArr();
+		int[] expected = getExpected();
+		for(int exp: expected){
+			System.out.println(exp);
+		}
+	}
 
+	private int[] getInputArr(){
+		int[] inputArr = new int[inputDimension];
+		try{
+			int readIn = 0;
+			reader.readLine(); //remove blank line
+			while(readIn < inputDimension){
+				String currLine = reader.readLine();
+				String[] inputs = currLine.split(" ");
+				for(String input: inputs){
+					inputArr[readIn] = Integer.parseInt(input);
+					readIn++;
+				}
+			}
+		}catch(Exception e){
+			System.out.println("ERROR: " + e);
+		}
+		
+		return inputArr;
+	}
+
+
+	private int[] getExpected(){
+		int[] expected = new int[outputDimension];
+		try{
+			reader.readLine(); //remove blank line
+			String expectedStr = reader.readLine();
+			String[] expectedArr = expectedStr.split(" ");
+			reader.readLine(); //remove character representation
+			for(int i = 0; i < outputDimension; i++){
+				expected[i] = Integer.parseInt(expectedArr[i]);
+			}
+		}catch(Exception e){
+			System.out.println("ERROR: " + e);
+		}
+
+		return expected;
 	}
 }
